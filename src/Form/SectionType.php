d8d4ce539ea7b530211093d1befe5bfd7624f0cd
Mgaren <basketeur009@gmail.com>
2021-03-13 19:28:42 +0100

    style verticalHistory

--- src/Form/SectionType.php
+++ src/Form/SectionType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Section;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -21,7 +22,7 @@ class SectionType extends AbstractType
                 'mapped' => false,
                 'label' => 'Image*',
             ])
-            ->add('description', TextType::class, [
+            ->add('description', TextareaType::class, [
                 'label' => 'Description*',
             ])
         ;
--- src/Form/VerticalHistoryType.php
+++ src/Form/VerticalHistoryType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\VerticalHistory;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -17,7 +18,7 @@ class VerticalHistoryType extends AbstractType
             ->add('titre', TextType::class, [
                 'label' => 'Date*',
             ])
-            ->add('description', TextType::class, [
+            ->add('description', TextareaType::class, [
                 'label' => 'Description*',
             ])
             ->add('fileimage', FileType::class, [
