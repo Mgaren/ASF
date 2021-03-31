f79642f45558af25679f818f58ac849be170e353
Mgaren <basketeur009@gmail.com>
2021-03-31 18:46:07 +0200

    add page history

--- src/Entity/History.php
+++ src/Entity/History.php
@@ -18,11 +18,6 @@ class History
     private int $id;
 
     /**
-     * @ORM\Column(type="string", length=255, nullable=true)
-     */
-    private ?string $name;
-
-    /**
      * @ORM\Column(type="string", length=255)
      */
     private string $description;
@@ -35,18 +30,6 @@ class History
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function getName(): ?string
-    {
-        return $this->name;
-    }
-
-    public function setName(?string $name): self
-    {
-        $this->name = $name;
-
-        return $this;
     }
 
     public function getDescription(): ?string
--- src/Form/HistoryType.php
+++ src/Form/HistoryType.php
@@ -6,7 +6,6 @@ use App\Entity\History;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,10 +14,6 @@ class HistoryType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', TextType::class, [
-                'required' => false,
-                'label' => 'Date'
-            ])
             ->add('description', TextareaType::class, [
                 'label' => 'Description*'
             ])
