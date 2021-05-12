3730e00398686129d2d4c9c69e13bd62a06f4bb8
Mgaren <basketeur009@gmail.com>
2021-05-12 15:07:39 +0200

    style page and responsive

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,6 +1,20 @@
 $(function () {
-    $('a').click(function () {
-        $('a').removeClass('active');
+    $('.nav_bar a').click(function () {
+        $('.nav_bar a').removeClass('active');
+        $(this).addClass('active');
+    });
+});
+
+$(function () {
+    $('.nav_barAsf a').click(function () {
+        $('.nav_barAsf a').removeClass('active');
+        $(this).addClass('active');
+    });
+});
+
+$(function () {
+    $('.nav_barSection a').click(function () {
+        $('.nav_barSection a').removeClass('active');
         $(this).addClass('active');
     });
 });
