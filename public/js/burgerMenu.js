64908cb72261026872ee9241d57c84ffab9b9693
Mgaren <basketeur009@gmail.com>
2021-05-25 12:37:35 +0200

    Add burgermenu for navbar

--- public/js/burgerMenu.js
+++ public/js/burgerMenu.js
@@ -1,19 +1,5 @@
-function openNav()
-{
-    if (window.matchMedia("(max-width: 1000px)").matches) {
-        document.querySelector('.sideNav').style.width = "35%";
-        document.querySelector('.nav').style.display = "flex";
-    }
-}
+$('#openButton').click(function (e) {
+    e.preventDefault();
+    $('body').toggleClass('with--sidebar')
+})
 
-function closeNav()
-{
-    document.querySelector('.sideNav').style.width = "0";
-    document.querySelector('.nav').style.display = "none";
-}
-
-const burgerButton = document.querySelector('.openButton');
-const closeButton = document.querySelector('.closeButton');
-
-burgerButton.addEventListener('click',openNav);
-closeButton.addEventListener('click',closeNav);
