6fd10f380b0e937fd23f12710051dfb0433a7e3e
Mgaren <basketeur009@gmail.com>
2021-05-27 18:01:29 +0200

    add style and responsive for nav-ASF

--- public/js/burgerMenu.js
+++ public/js/burgerMenu.js
@@ -3,3 +3,7 @@ $('#openButton').click(function (e) {
     $('body').toggleClass('with--sidebar')
 })
 
+$('#openButton-ASF').click(function (e) {
+    e.preventDefault();
+    $('body').toggleClass('with--sidebar-Asf')
+})
